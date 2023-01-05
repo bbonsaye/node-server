29adae1eaf1b9a1b53976da5058caacdc982f635
bbonsaye <bbonsaye@gmail.com>
2023-01-05 17:20:45 -0500

    added logic for logging details base on NODE_ENV being dev. or prod.

--- src/utils/middleware/errorHandlingMiddleware/errorLogger.js
+++ src/utils/middleware/errorHandlingMiddleware/errorLogger.js
@@ -1,8 +1,18 @@
 export default function errorLogger(error, req, res, next) {
-	console.log("---------------------");
+	console.log("-----------------");
 	console.log("within errorLogger");
-	// console.log(error);
-	console.log(error.errors);
-	console.log(error.statusCode);
+
+	if (process.env.NODE_ENV === "development") {
+		console.log("development logs:");
+		console.log(error);
+	}
+	//
+	else if (process.env.NODE_ENV === "production") {
+		console.log("production logs:");
+		console.log("-----------------");
+		console.log(error.message);
+		console.log(error.statusCode);
+	}
+
 	next(error);
 }
